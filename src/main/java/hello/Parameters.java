40b97d7726e27f725a16f63fa05030e23c7d6e48
NQ <pgs.nqinnovation2017@gmail.com>
2017-10-31 18:42:51 +0530

    Adding method for contribution rate change

--- src/main/java/hello/Parameters.java
+++ src/main/java/hello/Parameters.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -15,7 +16,9 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 })
 public class Parameters {
 
-    @JsonIgnore
+	@JsonProperty("empid")
+    private String empid;
+	@JsonIgnore
     private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
     @JsonAnyGetter
@@ -26,6 +29,16 @@ public class Parameters {
     @JsonAnySetter
     public void setAdditionalProperty(String name, Object value) {
         this.additionalProperties.put(name, value);
+    }
+    
+    @JsonProperty("empid")
+    public String getEmpid() {
+        return empid;
+    }
+
+    @JsonProperty("empid")
+    public void setEmpid(String empid) {
+        this.empid = empid;
     }
 
 }
--- src/main/java/hello/WebhookController.java
+++ src/main/java/hello/WebhookController.java
@@ -1,6 +1,11 @@
 package hello;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -16,14 +21,51 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebhookController {
 
 	@RequestMapping(method = RequestMethod.POST)
-    public @ResponseBody WebhookResponse webhook(@RequestBody String obj) throws JsonParseException, JsonMappingException, IOException{
+    public @ResponseBody WebhookResponse webhook(@RequestBody String obj) throws JsonParseException, JsonMappingException, IOException, ClassNotFoundException, SQLException{
 
         ObjectMapper mapper = new ObjectMapper();
         APIPojo apiPojo = mapper.readValue(obj, APIPojo.class);
         System.out.println(apiPojo.getResult().getAction());
-         if (apiPojo.getResult().getAction().equals("input.welcome")) {
-        	 return new WebhookResponse("Welcome Intent", "Welcome Intent");
+        System.out.println(apiPojo.getResult().getParameters().getEmpid());
+        
+        // For contribution rate change
+         if (apiPojo.getResult().getAction().equals("contributionRateChange")) {
+        	 Connection connection = getDatabaseConnection();
+        	 String empid = apiPojo.getResult().getParameters().getEmpid();
+        	 String plantype = execStmtForContributionRateChange(connection, empid);
+        	 if (plantype.equals("457(b)")) {
+        		String text = "Yes, you can change the contribution rate for a 457(b) plan using the Change Deferral link on the Plan Dashboard";
+        		return new WebhookResponse(text, text);
+        	 } else {
+        		 String text = "No you cannot change your contribution rate for Excess plan";
+        		 return new WebhookResponse(text, text);
+        	 }
+        		  	 
          }        
          return new WebhookResponse("Not Welcome Intent", " Not Welcome Intent");
     }
+	
+	public Connection getDatabaseConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("org.postgresql.Driver");
+		Connection connection = null;
+		
+		connection = DriverManager.getConnection(
+				"jdbc:postgresql://ec2-54-221-229-64.compute-1.amazonaws.com:5432/d5a4n902vanlmo?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", "kauctvpnsyodlt",
+				"b6e5fc9bbe5f88c42f2a83cdfd779fa8ead8fd6b4cae3da7c49674182e8d1939");
+		return connection;
+	}
+	
+	public String execStmtForContributionRateChange (Connection connection, String empid) throws SQLException {
+		Statement st =null;
+		st = connection.createStatement();
+		ResultSet rs = st.executeQuery("SELECT plantype from Employee where empid = " +empid);
+		String plantype = null;
+		while (rs.next()) {
+			plantype = rs.getString("plantype");
+		}
+		
+		return plantype;
+		
+		
+	}
 }
